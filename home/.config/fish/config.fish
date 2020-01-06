7cdb3d1dc8cdb90a692e0b21a1c1bdfa7b071f34
Andrew Dirksen <andrew@dirksen.com>
2020-01-06 15:15:21 -0800

    enable ctrl-l for lowercase word in fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -15,3 +15,11 @@ set -gx GOPATH ~/go
 set -Ux EDITOR emacs
 
 alias onch='ag -l | entr -rc'
+
+function bddap_key_bindings -d "key bindings for fish"
+    fish_default_key_bindings
+    bind --erase \el # disable __fish_list_current_token for alt-l
+    bind \es __fish_list_current_token # set it to alt-s instead
+    bind \el downcase-word
+end
+set -g fish_key_bindings bddap_key_bindings
