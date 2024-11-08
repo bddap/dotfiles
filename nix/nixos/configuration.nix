e2801fd83aca85219c939dfc9e009902b7251d62
Andrew Dirksen <andrew@dirksen.com>
2024-11-07 17:20:00 -0800

    cleanup configuration.nix

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -1,26 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { ... }:
-let
-  pkgs = import ../nix { };
-  user-packages = with pkgs; [ home-manager ];
+let pkgs = import ../nix { };
 in {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # disable tmpfs for /tmp, its limited size causes pain
-  boot.tmp.useTmpfs = false;
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # how did this get here? Shouldn't this be in hardware-configuration.nix?
+    initrd.luks.devices."luks-f78f0095-dcc0-4b51-b602-c0689384506f".device =
+      "/dev/disk/by-uuid/f78f0095-dcc0-4b51-b602-c0689384506f";
 
-  boot.initrd.luks.devices."luks-f78f0095-dcc0-4b51-b602-c0689384506f".device =
-    "/dev/disk/by-uuid/f78f0095-dcc0-4b51-b602-c0689384506f";
+    # disable tmpfs for /tmp, its limited size causes pain
+    tmp.useTmpfs = false;
+  };
+
   networking.hostName = "nixos"; # Define your hostname.
   hardware.bluetooth.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -28,19 +22,10 @@ in {
   # steam wants this
   hardware.opengl.driSupport32Bit = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -63,25 +48,26 @@ in {
   };
 
   services.xserver = {
-    # Enable the X11 windowing system.
     enable = true;
 
-    # Load nvidia driver for Xorg and Wayland
+    # does this belong in hardware-configuration.nix?
     videoDrivers = [ "nvidia" ];
 
-    # Enable the GNOME Desktop Environment.
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
 
-    # Configure keymap in X11
     layout = "us";
     xkbVariant = "";
+
+    # maybe not needed for trackpad inpu since it's enabled by default in most desktopManager?
+    libinput.enable = true;
+
+    displayManager.autoLogin.enable = true;
+    displayManager.autoLogin.user = "a";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -90,73 +76,38 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   programs.fish.enable = true;
   programs.nix-ld.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.a = {
     isNormalUser = true;
     description = "a";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = user-packages;
-
+    packages = [ pkgs.home-manager ];
     shell = pkgs.fish;
   };
 
-  fonts.packages = with pkgs; [ comic-mono ];
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "a";
+  fonts.packages = [ pkgs.comic-mono ];
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
+  environment.systemPackages = [ ]; # use home-manager instead
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
+  # Tried this, too much effort so gnome is manually configured for now
   # [org/gnome/desktop/input-sources]
   # xkb-options=['terminate:ctrl_alt_bksp', 'ctrl:swap_lalt_lctl']
-
   # [org/gnome/desktop/wm/keybindings]
   # toggle-fullscreen=['<Control><Super>f']
-
-  # # [org/gnome/nautilus/preferences]
-  # # migrated-gtk-settings=true
-
+  # [org/gnome/nautilus/preferences]
+  # migrated-gtk-settings=true
   # [org/gtk/gtk4/settings/file-chooser]
   # show-hidden=true
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
