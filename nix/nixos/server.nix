d64ed881830c7f865559722e272dcd2f5684280a
Andrew Dirksen <andrew@dirksen.com>
2026-03-09 18:09:46 -0700

    use randomly generated initial pasword hash

--- nix/nixos/server.nix
+++ nix/nixos/server.nix
@@ -1,8 +1,9 @@
 # Entry point for install-to-disk.
-# HOSTNAME env var is read at eval time.
+# HOSTNAME and INITIAL_PASSWORD_HASH env vars are read at eval time.
 { ... }:
 let
   hostname = builtins.getEnv "HOSTNAME";
+  initialPasswordHash = builtins.getEnv "INITIAL_PASSWORD_HASH";
 in {
   imports = [
     ./common.nix
@@ -12,6 +13,8 @@ in {
   networking.hostName =
     if hostname != "" then hostname
     else throw "HOSTNAME env var must be set";
+
+  users.users.a.initialHashedPassword = initialPasswordHash;
 
   users.users.bot = {
     isNormalUser = true;
